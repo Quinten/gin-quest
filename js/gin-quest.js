bc7797c94fa07c256f3229386141f46f7ba89a05
Quinten <quintenclause@hotmail.com>
2014-12-08 13:42:41 +0100

    created a basic engine

--- js/gin-quest.js
+++ js/gin-quest.js
@@ -1,3 +1,50 @@
-window.onload = function () {
-  alert('Gin!');
+/* this will be our core engine  */
+
+var ngn = {};
+ngn.setup = function () {
+  if (!window.innerWidth) {
+    setTimeout(ngn.setup, 500);
+    return;
+  }
+  
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+                                    window.mozRequestAnimationFrame ||
+                                    window.msRequestAnimationFrame ||
+                                    window.oRequestAnimationFrame ||
+                                    function (callback) {
+                                      return window.setTimeout(callback, 17);
+                                    });
+  }
+
+  var canvas = document.getElementById('canvas'), context = canvas.getContext('2d');
+
+  if(!context){
+    return;
+  }
+
+  window.addEventListener('resize', onR, false);
+  onR();
+  function onR () {
+    canvas.width = (window.innerWidth < 1024) ? window.innerWidth : 1024;
+    canvas.height = (window.innerHeight < 1024) ? window.innerHeight : 1024;
+  }
+
+  (function onF (){
+    window.requestAnimationFrame(onF, canvas);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  
+    context.fillStyle = "#00ff00";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fill();
+  }());
+      
 };
+    
+if(document.readyState === 'complete'){
+  ngn.setup();
+} else {
+  window.onload = function (){
+    ngn.setup();
+  };
+}
