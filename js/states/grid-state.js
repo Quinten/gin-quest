2b39feb36dcc727accb03bf74137b2225782d4e4
Quinten <quintenclause@hotmail.com>
2014-12-08 14:47:39 +0100

    Created grid-state.js

---
+++ js/states/grid-state.js
@@ -0,0 +1,11 @@
+var gridState = function () {
+
+  this.init = function () {
+    console.log('grid initialized');
+  };
+  
+  this.render = function (context) {
+    console.log('grid rendering');
+  };
+
+};
