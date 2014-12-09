83aa7eb77d08d0186d7697d37a83cfb9136935f3
Quinten <quintenclause@hotmail.com>
2014-12-09 21:06:24 +0100

    Update grid.js

--- js/astar/grid.js
+++ js/astar/grid.js
@@ -4,7 +4,7 @@ var Grid = function () {
   this.nodes = [];
   this.numCols = 32;
   this.numRows = 32;
-  
+
   this.init = function (numCols, numRows) {
     if (numCols != undefined){
       this.numCols = numCols;
@@ -12,5 +12,28 @@ var Grid = function () {
     if (numRows != undefined) {
       this.numRows = numRows;
     }
+    for(var i = 0; i < this.numCols; i++){
+      this.nodes[i] = [];
+      for(var j = 0; j < this.numRows; j++){
+        this.nodes[i][j] = new Node(i, j);
+      }
+    }
   };
+
+  this.getNode = function (x, y) {
+    return this.nodes[x][y];
+  };
+
+  this.setStartNode = function(x, y){
+    this.startNode = this.nodes[x][y];
+  };
+
+  this.setEndNode = function(x, y){
+    this.endNode = this.nodes[x][y];
+  };
+
+  this.setWalkable = function(x, y, val){
+    this.nodes[x, y].walkable = val;
+  };
+
 };
