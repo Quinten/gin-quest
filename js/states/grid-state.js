2851f2b4226d8ee28b0448b810b9a1ba6704568a
Quinten <quintenclause@hotmail.com>
2014-12-09 16:45:00 +0100

    Update grid-state.js

--- js/states/grid-state.js
+++ js/states/grid-state.js
@@ -17,7 +17,7 @@ var gridState = function () {
       context.moveTo(0, y * 64);
       context.lineTo(2048, y * 64);
     }
-    context.strokeStyle = "#99cc00";
+    context.strokeStyle = "#669900";
     context.stroke();
     context.fillStyle = "#ff3300";
     context.fillRect(1024-32, 1024-32, 64, 64);
