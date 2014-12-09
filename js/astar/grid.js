809b1e7f530e95408d855ae6c44467db6acbc9f9
Quinten <quintenclause@hotmail.com>
2014-12-09 20:17:59 +0100

    Update grid.js

--- js/astar/grid.js
+++ js/astar/grid.js
@@ -1,2 +1,12 @@
-function grid() {
-}
+var grid = function () {
+  this.startNode = null;
+  this.endNode = null;
+  this.nodes = [];
+  this.numCols = 32;
+  this.numRows = 32;
+  
+  this.init = function (nCols = 32, nRows = 32) {
+    this.numCols = nCols;
+    this.numRows = nRows;
+  };
+};
