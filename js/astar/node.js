f38c70dfcbb8aef55166daca83528da45b5a0953
Quinten <quintenclause@hotmail.com>
2014-12-09 20:42:46 +0100

    Create node.js

---
+++ js/astar/node.js
@@ -0,0 +1,9 @@
+var Node = function (x, y) {
+  this.x = (x == undefined) ? 0 : x;
+  this.y = (y == undefined) ? 0 : y;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+  this.walkable = true;
+  parent = null;
+}
