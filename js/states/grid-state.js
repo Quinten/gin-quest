7bccb1a6f5bb7f773925c06ee00cdb4011dc9c7e
Quinten <quintenclause@hotmail.com>
2014-12-09 11:34:40 +0100

    Draw a grid

--- js/states/grid-state.js
+++ js/states/grid-state.js
@@ -5,7 +5,20 @@ var gridState = function () {
   };
   
   this.render = function (context) {
-    console.log('grid rendering');
+    //console.log('grid rendering');
+    context.save();
+    context.beginPath();
+    for (var x = 1; x < 32; x++ ) {
+      context.moveTo(x * 64, 0);
+      context.lineTo(x * 64, 2048);
+    }
+    for (var y = 1; y < 32; y++ ) {
+      context.moveTo(0, y * 64);
+      context.lineTo(2048, y * 64);
+    }
+    context.stroke();
+    context.closePath();
+    context.restore();
   };
   
   this.destroy = function () {
