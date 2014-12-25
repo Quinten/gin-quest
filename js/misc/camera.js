7dc7fc7b6affe8ecdc7dbe4e67adc47588b2b009
Quinten Clause <quintenclause@hotmail.com>
2014-12-25 21:27:03 +0100

    Update camera.js

--- js/misc/camera.js
+++ js/misc/camera.js
@@ -2,6 +2,7 @@ var Camera = function (x, y) {
   this.x = (x == undefined) ? 0 : x;
   this.y = (y == undefined) ? 0 : y;
   
+  // follow a sprite
   this.follow = function (sprite) {
     this.x += (sprite.centerX - this.x) / 12;
     this.y += (sprite.centerY - this.y) / 12;
